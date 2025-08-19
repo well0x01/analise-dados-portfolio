ccf12b280f67048c19bcd13b81c8047b8160f8c6
well0x01 <77464680+well0x01@users.noreply.github.com>
2025-08-19 00:09:22 -0300

    Update analise-vendas.sql

--- sql-projects/adventureworks/analise-vendas.sql
+++ sql-projects/adventureworks/analise-vendas.sql
@@ -1,1 +1,20 @@
+-- ============================================
+-- 📊 Produtos Mais Vendidos - AdventureWorksDW2022
+-- Objetivo: identificar os produtos com maior quantidade vendida.
+-- Recursos: JOIN + SUM + GROUP BY + ORDER BY
+-- Insight esperado: destacar os produtos mais populares e estratégicos.
+-- ============================================
+
+USE AdventureWorksDW2022;
+
+SELECT TOP 10
+    P.EnglishProductName AS [PRODUTO],
+    SUM(FIS.SalesAmount) AS [FATURAMENTO TOTAL]
+FROM
+    FactInternetSales AS FIS
+INNER JOIN DimProduct AS P ON P.ProductKey = FIS.ProductKey
+GROUP BY
+    P.EnglishProductName
+ORDER BY
+    [FATURAMENTO TOTAL] DESC;
 
