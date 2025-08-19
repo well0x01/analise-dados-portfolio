023235f5ce9229deb19a093c697b465e1f8c167d
well0x01 <77464680+well0x01@users.noreply.github.com>
2025-08-19 00:07:36 -0300

    Update ranking-lojas.sql

--- sql-projects/contoso/ranking-lojas.sql
+++ sql-projects/contoso/ranking-lojas.sql
@@ -1,1 +1,20 @@
+-- ============================================
+-- 🏬 Ranking de Lojas - ContosoRetailDW
+-- Objetivo: listar as lojas com maior volume de vendas.
+-- Recursos: JOIN + SUM + ORDER BY DESC + TOP
+-- Insight esperado: destacar as lojas mais rentáveis.
+-- ============================================
+
+USE ContosoRetailDW;
+
+SELECT TOP 10
+    ST.StoreName AS [LOJA],
+    SUM(S.SalesAmount) AS [FATURAMENTO TOTAL]
+FROM
+    FactSales AS S
+INNER JOIN DimStore AS ST ON ST.StoreKey = S.StoreKey
+GROUP BY
+    ST.StoreName
+ORDER BY
+    [FATURAMENTO TOTAL] DESC;
 
