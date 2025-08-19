67e39d1ded1bd47cc662e0b8cfc5f8a083166e1c
well0x01 <77464680+well0x01@users.noreply.github.com>
2025-08-19 00:06:57 -0300

    Update vendas-por-categoria.sql

--- sql-projects/contoso/vendas-por-categoria.sql
+++ sql-projects/contoso/vendas-por-categoria.sql
@@ -1,1 +1,22 @@
+-- ============================================
+-- 📊 Vendas por Categoria - ContosoRetailDW
+-- Objetivo: calcular o faturamento total por categoria de produto.
+-- Recursos: JOIN + GROUP BY + SUM + ORDER BY
+-- Insight esperado: identificar as categorias mais lucrativas.
+-- ============================================
+
+USE ContosoRetailDW;
+
+SELECT
+    PC.ProductCategoryName AS [CATEGORIA],
+    SUM(S.SalesAmount) AS [FATURAMENTO TOTAL]
+FROM
+    FactSales AS S
+INNER JOIN DimProduct AS P ON P.ProductKey = S.ProductKey
+INNER JOIN DimProductSubcategory AS PS ON PS.ProductSubcategoryKey = P.ProductSubcategoryKey
+INNER JOIN DimProductCategory AS PC ON PC.ProductCategoryKey = PS.ProductCategoryKey
+GROUP BY
+    PC.ProductCategoryName
+ORDER BY
+    [FATURAMENTO TOTAL] DESC;
 
